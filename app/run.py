1a5fd72d73aaa08f772e6fe3b047e1fa332791a4
Teresiah Githua <terrywambo2018@gmail.com>
2022-05-09 07:31:27 +0300

    ft:create pitch list dictionary

--- app/run.py
+++ app/run.py
@@ -1,19 +1,37 @@
-from flask import Flask
-
+from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route("/home")
+pitches = [
+    {
+            'author': 'Teresiah',
+            'title':  'Pitch',
+            'content': 'My first pitch',
+            'date_created': 'May 8, 2022'
+        },
+        {
+            'author': 'Wambui',
+            'title':  'Pitch2',
+            'content': 'My second pitch',
+            'date_created': 'May 9, 2022'
+        
+        }
+]
+
+
+@app.route("/")
 def home():
-    return "<h1>Home page</h1>"
+    return render_template('home.html', pitches=pitches)
 
 @app.route("/register")
 def register():
-    return "<h1>Register page</h1>"
+    return render_template('register.html')
+
 
 
 @app.route("/login")
 def login():
-    return "<h1>Login page</h1>"
+    return render_template('login.html')
+
 
 
 
