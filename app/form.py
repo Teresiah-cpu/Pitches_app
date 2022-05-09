ef9a7f874cd1ea407841bb6121aa2403b3818fa7
Teresiah Githua <terrywambo2018@gmail.com>
2022-05-09 15:06:05 +0300

    ft:add form submission button

--- app/form.py
+++ app/form.py
@@ -1,13 +1,13 @@
 import email
 from logging.config import valid_ident
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
-from wtforms.validators import DataRequired,Length,Email
+from wtforms import StringField, PasswordField, SubmitField 
+from wtforms.validators import DataRequired,Length,Email, EqualTo
 
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(),Length(min=2, max=20)])
     email  = StringField('Email', validators=[DataRequired(),Email()])
     password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm Password', validators=[DataRequired()])
-
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('Password')])
+    submit = SubmitField('Sign Up')
