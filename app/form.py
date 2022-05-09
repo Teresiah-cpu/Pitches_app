03aab78837713a0a7ac20dd96f7831202a4d986e
Teresiah Githua <terrywambo2018@gmail.com>
2022-05-10 00:17:34 +0300

    ft:validate the form

--- app/form.py
+++ app/form.py
@@ -4,6 +4,8 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField 
 from wtforms.validators import DataRequired,Length,Email, EqualTo
 
+# from app import app
+
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(),Length(min=2, max=20)])
@@ -19,3 +21,5 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember me')
     submit = SubmitField('Login')
+
+# app.run(debug=True)    
