d1b13892abd99406a593ce8f418699a1f3693183
Teresiah Githua <terrywambo2018@gmail.com>
2022-05-09 14:57:47 +0300

    add form input fields

--- app/form.py
+++ app/form.py
@@ -1,7 +1,13 @@
+import email
+from logging.config import valid_ident
 from flask_wtf import FlaskForm
-from wtforms import StringField
-from wtforms.validators import DataRequired,Length
+from wtforms import StringField, PasswordField
+from wtforms.validators import DataRequired,Length,Email
 
 
 class RegistrationForm(FlaskForm):
-    username = StringField("Username", validators=[DataRequired(),Length(min=2, max=20)])
+    username = StringField('Username', validators=[DataRequired(),Length(min=2, max=20)])
+    email  = StringField('Email', validators=[DataRequired(),Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired()])
+
